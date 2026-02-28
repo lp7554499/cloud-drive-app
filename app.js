848cb3ecfec9ae146aaa7b01eaf01ebcea605780
lp7554499 <lp7554499@gmail.com>
2026-02-28 12:44:27 +0530

    home added

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 const userRoutes = require('./routes/userRoutes')
+const homeRoutes = require('./routes/homeRoutes')
 const { body, validationResult } = require('express-validator');
 const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
 dotenv.config()
 
 const dbConnect = require('./config/db')
@@ -11,10 +13,12 @@ dbConnect()
 
 
 app.set('view engine','ejs')
+app.use(cookieParser())
 
 //this are middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/',homeRoutes)
 app.use('/user',userRoutes)
 
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,31 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    
+    username:{
+        type:String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        minlength: [3, 'username must be at least 3 characters  long']
+    },
+    email:{
+        type:String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        minlength: [13, 'email must be at least 13 characters  long']
+    },
+    password:{
+        type:String,
+        required: true,
+        trim: true,
+        minlength: [5, 'password must be at least 5 characters  long']
+    },
 })
+
+
+const userModel = mongoose.model('user',userSchema)
+
+module.exports = userModel
